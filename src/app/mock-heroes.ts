4807a1017f21e59d9c82bfb6a04d61e012381830
Felipe Gutierrez <feguti4@gmail.com>
2022-05-27 17:42:03 -0300

    Updating the application to display a list of heroes with a detail view of the selected hero when clicked

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 12, name: ' Super Pedro'},
+  {id: 13, name: ' Oliver Sykes'},
+  {id: 14, name: ' Will Smith'},
+  {id: 15, name: ' Pingu'},
+  {id: 16, name: ' AnGeL oF tHe NiGhT'},
+  {id: 17, name: ' Totodile de tênis'},
+  {id: 18, name: ' Diluc'},
+  {id: 19, name: ' Piui Tic Tac'},
+  {id: 20, name: ' Sekiro'},
+];
