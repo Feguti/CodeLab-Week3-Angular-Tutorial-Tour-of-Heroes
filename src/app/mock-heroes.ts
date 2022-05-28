c589bf9c67ac9ac298cd10dd96843cc0d3beba09
Felipe Gutierrez <feguti4@gmail.com>
2022-05-28 18:32:13 -0300

    Adding navigation functions: creating a hero dashboard, hero details route and and navigation between pages. Also updating css files

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,11 +1,11 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  {id: 12, name: ' Super Pedro'},
-  {id: 13, name: ' Oliver Sykes'},
+  {id: 12, name: ' Oliver Sykes'},
+  {id: 13, name: ' Super Pedro'},
   {id: 14, name: ' Will Smith'},
-  {id: 15, name: ' Pingu'},
-  {id: 16, name: ' AnGeL oF tHe NiGhT'},
+  {id: 15, name: ' AnGeL oF tHe NiGhT'},
+  {id: 16, name: ' Pingu'},
   {id: 17, name: ' Totodile de tênis'},
   {id: 18, name: ' Diluc'},
   {id: 19, name: ' Piui Tic Tac'},
