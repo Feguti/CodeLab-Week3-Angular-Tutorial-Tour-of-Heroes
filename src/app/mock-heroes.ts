061478d02070b013725d13275991aae6bbe8560e
Felipe Gutierrez <feguti4@gmail.com>
2022-05-29 15:58:47 -0300

    Creating submit new hero form

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,13 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  {id: 12, name: ' Oliver Sykes'},
-  {id: 13, name: ' Super Pedro'},
-  {id: 14, name: ' Will Smith'},
-  {id: 15, name: ' AnGeL oF tHe NiGhT'},
-  {id: 16, name: ' Pingu'},
-  {id: 17, name: ' Totodile de tênis'},
-  {id: 18, name: ' Diluc'},
-  {id: 19, name: ' Piui Tic Tac'},
-  {id: 20, name: ' Sekiro'},
+  {id: 12, name: ' Oliver Sykes', power: 'Sound breathing: KingSlayer style'},
+  {id: 13, name: ' Super Pedro', power: 'Super strength'},
+  {id: 14, name: ' Will Smith', power: 'The Slap'},
+  {id: 15, name: ' AnGeL oF tHe NiGhT', power: 'Cinerária'},
+  {id: 16, name: ' Pingu', power: 'Ice breathing: Northern kingdoms style'},
+  {id: 17, name: ' Totodile de tênis', power: 'He is cute'},
+  {id: 18, name: ' Diluc', power: 'Sun breathing: Fenix rage style'},
+  {id: 19, name: ' Piui Tic Tac', power: 'Chocolate'},
+  {id: 20, name: ' Sekiro', power: 'immortal blade techniques'},
 ];
